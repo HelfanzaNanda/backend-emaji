0b7072f798807e7140ae68f2a56723c342c37caf
HelfanzaNanda <elvannnd@gmail.com>
2021-01-10 15:14:51 +0700

    request user

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,7 +25,7 @@ class UserRequest extends FormRequest
     {
         return [
             "name" => ['required'],
-            "email" => ['required', 'email', 'unique:users,email,' . optional($this->user)->id],
+            "email" => ['required', 'email', 'unique:users,email,']. optional($this->user)->id,
             'role' => ['required', 'in:penguji,pengawas']
         ];
     }
